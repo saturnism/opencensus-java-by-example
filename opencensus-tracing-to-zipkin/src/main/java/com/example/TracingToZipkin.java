1d24ca6700daf2552df26e58315d35bab13e5b88
Ray Tsang <rayt@google.com>
2018-08-02 22:36:57 -0400

    updated

--- opencensus-tracing-to-zipkin/src/main/java/com/example/TracingToZipkin.java
+++ opencensus-tracing-to-zipkin/src/main/java/com/example/TracingToZipkin.java
@@ -11,18 +11,18 @@ import io.opencensus.trace.samplers.Samplers;
 
 public class TracingToZipkin {
 	public static void main(String[] args) {
-		// Get the global singleton Tracer object
-		Tracer tracer = Tracing.getTracer();
-
-		// Configure exporter to export traces to Zipkin
+		// 1. Configure exporter to export traces to Zipkin.
 		ZipkinTraceExporter.createAndRegister("http://localhost:9411/api/v2/spans", "tracing-to-zipkin-service");
 
-		// Configure 100% sample rate
+		// 2. Configure 100% sample rate, otherwise, few traces will be sampled.
 		TraceConfig traceConfig = Tracing.getTraceConfig();
 		TraceParams activeTraceParams = traceConfig.getActiveTraceParams();
 		traceConfig.updateActiveTraceParams(activeTraceParams.toBuilder().setSampler(Samplers.alwaysSample()).build());
 
-		// Create a scoped span, a scoped span will automatically end when closed.
+		// 3. Get the global singleton Tracer object.
+		Tracer tracer = Tracing.getTracer();
+
+		// 4. Create a scoped span, a scoped span will automatically end when closed.
 		// It implements AutoClosable, so it'll be closed when the try block ends.
 		try (Scope scope = tracer.spanBuilder("main").startScopedSpan()) {
 			System.out.println("About to do some busy work...");
@@ -31,13 +31,17 @@ public class TracingToZipkin {
 			}
 		}
 
+		// 5. Gracefully shutdown the exporter, so that it'll flush queued traces to Zipkin.
 		Tracing.getExportComponent().shutdown();
 	}
 
 	private static void doWork(int i) {
-		// Get the global singleton Tracer object
+		// 6. Get the global singleton Tracer object.
 		Tracer tracer = Tracing.getTracer();
 
+		// 7. Start another span. If antoher span was already started, it'll use that span as the parent span.
+		// In this example, the main method already started a span, so that'll be the parent span, and this will be
+		// a child span.
 		try (Scope scope = tracer.spanBuilder("doWork").startScopedSpan()) {
 			// Simulate some work.
 			try {
